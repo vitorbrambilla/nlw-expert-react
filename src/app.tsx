7cccfba09ac5f89355f26ae2dd1e7be6e9357614
Vitor <vitor.reis@crmservices.com.br>
2024-02-06 18:57:42 -0300

    feat: new note and note card component

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,11 @@ import logo from "./assets/logo.svg";
 import { NewNoteCard } from "./components/new-note-card";
 import { NoteCard } from "./components/note-card";
 
+const note = {
+  date: new Date(),
+  content: "Hellooo",
+};
+
 export function App() {
   return (
     <div className="mx-auto my-12 max-w-6xl space-y-6">
@@ -20,9 +25,8 @@ export function App() {
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
         <NewNoteCard />
 
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
+        <NoteCard note={note} />
+        <NoteCard note={note} />
       </div>
     </div>
   );
